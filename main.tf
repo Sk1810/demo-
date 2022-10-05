1611dcd5791fa324f16bfb2ded69ff290432e647
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-05 23:29:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 resource "aws_acm_certificate" "acm-resource" {
-  domain_name       = "example.com"
+  domain_name       = "var.domain-name"
   validation_method = "DNS"
 
   tags = {
     Environment = "test"
   }
+  
+resource "aws_cloudfront_origin_access_identity" "cloud-oai" {
+  comment = "var.bucket-name"
+}  
 
 resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-tf-test-bucket"
+  bucket = "var.bucket-name"
 
   tags = {
     Name        = "My bucket"
@@ -15,28 +19,24 @@ resource "aws_s3_bucket" "my-bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = aws_s3_bucket.my-bucket.id
   acl    = "private"
 }
   
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket                  = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "s3-bucket-public" {
+  bucket                  = aws_s3_bucket.my-bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }  
 
-data "aws_iam_policy_document" "allow_access_from_another_account" {
+data "aws_iam_policy_document" "bucket-policy" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["123456789012"]
+      identifiers = ["aws_cloudfront_origin_access_identity.cloud-oai.iam.arn"]
     }
 
     actions = [
@@ -44,16 +44,13 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
       "s3:ListBucket",
     ]
 
-    resources = [
-      aws_s3_bucket.example.arn,
-      "${aws_s3_bucket.example.arn}/*",
-    ]
+    resources = ["${aws_s3_bucket.my-bucket.arn}/*"}
   }
 }  
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my-bucket.id
+  policy = data.aws_iam_policy_document.bucket-policy.json
 }
   
 resource "aws_cloudfront_distribution" "s3_distribution" {

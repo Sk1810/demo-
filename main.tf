b8560c2bef747df29de5172c6384be84e4d5fe38
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-06 14:41:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,5 +102,6 @@ resource "aws_route53_record" "www" {
   zone_id           = aws_route53_zone.primary.zone_id
   name              = "www.skazure.com"
   type              = "A"
+  ttl               = 300
 }  
   

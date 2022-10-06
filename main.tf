99e1887b29e96062b76b6f2aaf3661295ee96621
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-06 15:17:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,20 +91,18 @@ viewer_certificate {
  
  resource "aws_route53_zone" "primary" {
   provider          = aws.us_region 
-  name              = "www.sk-aws.com"
+  name              = "sk-aws.com"
 }
   
 resource "aws_route53_record" "www" {
-  provider          = aws.us_region
-  zone_id           = aws_route53_zone.primary.zone_id
-  name              = "www.skazure.com"
-  type              = "A"
-  ttl               = 300
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "azure"
+  type    = "A"
 
-alias {
+  alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
-}   
+}
   

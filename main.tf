65b006fd18a8409cff0398ea4b707fdb418f10d8
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-06 12:25:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "bucket-policy" {
       "s3:ListBucket",
     ]
 
-resources = ["${aws_s3_bucket.my-bucket.arn}/*"}
+resources = ["${aws_s3_bucket.my-bucket.arn}/*"]
   }
 }  
 

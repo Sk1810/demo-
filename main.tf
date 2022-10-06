efd2671eb4ed0be6ee60d8f005d68b18fb370932
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-06 12:54:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.my-bucket.bucket_regional_domain_name
-    origin_id   = sk-azure
+    origin_id   = "S3-${aws_s3_bucket.my-bucket}"
 
     s3_origin_config {
       origin_access_identity = "origin-access-identity/cloudfront/${aws_cloudfront_origin_access_identity.cloud-oai.id}"
@@ -69,7 +69,7 @@ enabled = true
 default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "sk-azure"  
+    target_origin_id       = "S3-${aws_s3_bucket.my-bucket}"  
     viewer_protocol_policy = "redirect-to-https"
 }  
   
